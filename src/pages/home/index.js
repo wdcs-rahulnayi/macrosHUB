fb1d06ef8ab89172f7b6a653dcfde95470e54455
rahul nayi <rahul.nayi@codezeros.com>
2024-01-23 16:08:46 +0530

    first commit

---
+++ src/pages/home/index.js
@@ -0,0 +1,16 @@
+
+
+const Home = () => {
+    return (
+        <>
+            <div className="flex min-h-screen flex-col items-center justify-between p-24 ">
+                <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-lg lg:flex">
+                    Welcome to the macrosHUB
+                </div>
+            </div>
+
+        </>
+    )
+};
+
+export default Home;
---
+++ src/pages/signup/index.js
@@ -0,0 +1,11 @@
+import Signup from "@/components/Signup";
+const SignupPage = () => {
+    return (
+      <>
+      <Signup />
+      </>
+    )
+  };
+  
+export default SignupPage;
+  
